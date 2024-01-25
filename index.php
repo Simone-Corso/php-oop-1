14cc290485e436c27884e5806d7df6a247e22b09
Simone-Corso <scorso26@gmail.com>
2024-01-25 15:23:35 +0100

    ho creato due istanze con nome movieFirst and movieTwo

--- index.php
+++ index.php
@@ -11,4 +11,6 @@ include 'movie.php';
  * creiamo un istanza del movie
  */
 
- 
+ $movieFirst = new Movie("Fast and Furious", "Azione", 2001);
+
+ $movieTwo = new Movie("2 Fast 2 Furious", "Azione", 2003);
